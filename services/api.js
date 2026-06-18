f07c01005564d3f6457d48da973eb9b2ce2e98b3
Harshit Singh <harshit.singh.hs9454@gmail.com>
2026-06-19 01:46:09 +0530

    added backend url of render

--- services/api.js
+++ services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_URL,
+  baseURL: "https://roomit-backend-z03a.onrender.com",
 });
 
 export default api;
